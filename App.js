2e502ffdfde2fcdce3b2589d02d50f3dd7717473
sebastianzalazar <113072897+sebastianzalazar@users.noreply.github.com>
2023-03-31 18:10:16 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">word
+        
+      </header>
+      <section className="section1">claro
+
+      </section>
+      <section className="section2">bay
+
+      </section>
+      <footer className="footer">hello
+
+      </footer>
+    </div>
+  );
+}
+
+export default App;
